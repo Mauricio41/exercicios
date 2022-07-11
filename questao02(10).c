63ab27042f8e44d0585820c6d166663eba765b89
Maurício Rômulo <105665865+Mauricio41@users.noreply.github.com>
2022-07-11 09:05:16 -0300

    Update questao02(10).c

--- questao02(10).c
+++ questao02(10).c
@@ -1,1 +1,16 @@
+#include<stdio.h>
 
+int main(){
+    char frase[200];
+    int quantid,total=0;
+    printf("\tComtador de caracteres:\n");
+    printf("Digite uma palavra:\n");
+    scanf("%s",&frase);
+  
+    while( frase[total] != '\0'){
+        total++;
+    }
+  
+    quantid = total;
+    printf("\n\nA apalavra %s tem %d caracteres", frase,quantid);
+}
