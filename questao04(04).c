8df446771f3fd71300cd97e35f2cbff921856425
Maurício Rômulo <105665865+Mauricio41@users.noreply.github.com>
2022-07-11 09:45:14 -0300

    Update questao04(04).c

--- questao04(04).c
+++ questao04(04).c
@@ -1,1 +1,20 @@
+#include<stdio.h>
 
+float area(float h,float b){
+    float m_area = (b*h)/2;
+    return m_area;
+}
+
+int main(){
+    float tri_h,tri_b,result;
+  
+    printf("Informe a medida da altura do triangulo:\n");
+    scanf("%f",&tri_h);
+    printf("Informe a medida da base do triangulo:\n");
+    scanf("%f",&tri_b);
+  
+    result = area(tri_h,tri_b);
+  
+    printf("A area do triangulo é %.2f",result);
+    
+}
