8ecc685d21eb83c5654d927ea5079bb3e879e090
Maurício Rômulo <105665865+Mauricio41@users.noreply.github.com>
2022-07-11 08:33:15 -0300

    Update questao01(08).c

--- questao01(08).c
+++ questao01(08).c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
 int main(){
-
+  float v1[20],v2[20];
+  int a,b,c;
+  
+  for(a=0,a<20;a++){
+    printf("Digite o %dº valor:\n");
+    scanf("%f",&v1[a]);
+  }
+  for(b=19;b>=0;b--){
+    v2[b]=v1[b];
+  }
+  
+  printf("O vetor ficou assim");
+  for(c=0;c<20;c++){
+    printf("%.1f",v2[c]);
+  }
+  
 }
