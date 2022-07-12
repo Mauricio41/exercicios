607050d5408f75e338e61485cbaafd8712006abf
Maurício Rômulo <105665865+Mauricio41@users.noreply.github.com>
2022-07-12 20:35:57 -0300

    Update questao05(03).c

--- questao05(03).c
+++ questao05(03).c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <math.h>
 
+float are_c(float R){
+    float result,pi=3.14;
+    result=pi*(pow(R,2));
+    return result;
+}
+
+int main(void){
+	float raio, area;
+	
+	printf("RAIO = "); 
+	scanf("%f", &raio);
+ 
+	area = are_c(raio);
+ 
+	printf("AREA = %.2f m2 \n", area);
+ 
+}
