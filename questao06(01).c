721314841d66b9bded8d7cf0933eb3c1a42e84ef
Maurício Rômulo <105665865+Mauricio41@users.noreply.github.com>
2022-07-12 21:02:04 -0300

    Update questao06(01).c

--- questao06(01).c
+++ questao06(01).c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <math.h>
 
+float pott(float X,float Y){
+    float result=X;
+    int a;
+    for(a=2;a<=Y;a++){
+        result+=result*result;
+    }
+    return result;
+}
+
+
+int main(void){
+	float x,y,resultado,cont,cont1;
+	float a,b;
+	
+	printf("Valor de X = "); 
+	scanf("%f", &x);
+	printf("\nValor de Y = "); 
+	scanf("%f", &y);
+	printf("\nValor de A maior que 0(ZERO) = "); 
+	scanf("%f", &a);
+	printf("\nValor de B maior que 0(ZERO) = "); 
+	scanf("%f", &b);
+    
+    if(a>0 && b>0){
+        cont=(x-y);
+        cont1=(a+b);
+        resultado=pott(x,a)+pott(y,b)+pott(cont,cont1);
+    }else{
+        printf("Tente digitar apenas numeros maiores que ZERO para A e B");
+    }
+
+ 
+	printf("Resultado = %.2f \n", resultado);
+ 
+}
