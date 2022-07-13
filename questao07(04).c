8dc2aaf4a7e392f2bf015e4ceb65dffe83b59704
Maurício Rômulo <105665865+Mauricio41@users.noreply.github.com>
2022-07-12 21:36:36 -0300

    Update questao07(04).c

--- questao07(04).c
+++ questao07(04).c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+
+int main(int argc, char const *argv[]){
+  int a;
+  int b;
+  int *cont;
+
+  printf("\n\tDigite o valor de A:");
+  scanf("%d",&a);
+  printf("\n\tDigite o valor de B:");
+  scanf("%d",&b);
+
+  printf("\nOs valores ficaram assim:\nA = %d\nB = %d",a,b);
+
+  *cont = a;
+  a = b;
+  b = *cont;
+
+  printf("\n\nOs valores inversos:\n A = %d\nB = %d",a,b);
+
+}
